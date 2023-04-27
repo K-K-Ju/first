2055c49d5b74a7537adc356c1aa9c6a5f37707dd
K-K-Ju <andrunivermail@gmail.com>
2023-04-28 00:54:34 +0300

    Added: - health_check endpoint which returns 200 if server worlks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", name)
+}
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+//#[actix_web::main] // or
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(greet)
+            .service(health_check)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
