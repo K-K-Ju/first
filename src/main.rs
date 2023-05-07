f0d0908a15e622990e5b5e870b862be917272c59
K-K-Ju <andrunivermail@gmail.com>
2023-05-07 18:52:32 +0300

    Added - once_cell crate This crate is used for initialising logger during the test to prevent multiple invocations of global setting of logger. - get_subscriber accepts +argument which represents downstream of tracing data

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,11 @@ use first::telemetry::{init_subscriber, get_subscriber};
 //#[actix_web::main] // or
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber(
+        "zero2prod".into(),
+        "info".into(),
+        std::io::stdout
+    );
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,6 +2,21 @@ use std::net::TcpListener;
 use sqlx::{Connection, Executor, PgConnection, PgPool, Row};
 use uuid::Uuid;
 use first::configuration::{DatabaseSettings, get_configuration};
+use once_cell::sync::Lazy;
+use first::telemetry::{get_subscriber, init_subscriber};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
+});
 
 pub struct TestApp {
     address: String,
@@ -9,6 +24,10 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    // The first time `initialize` is invoked the code in `TRACING` is executed.
+    // All other invocations will instead skip execution.
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
